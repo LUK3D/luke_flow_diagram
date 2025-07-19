f13199a1ef160e0ba47ec7421db8858e7ddd3e08
Luk3D <filipelukebana@gmail.com>
2025-07-19 13:21:17 -0300

    feat: Implement Change and ChangeStack classes for undo/redo functionality

---
+++ lib/undo/simple_stack.dart
@@ -0,0 +1,36 @@
+import 'change.dart';
+import 'undo_stack.dart';
+
+class SimpleStack<T> extends ChangeStack {
+  /// Simple stack for keeping track of changes and easy callback for new state changes
+  SimpleStack(this._state, {super.limit, this.onUpdate}) {
+    if (onUpdate != null) {
+      onUpdate!(_state);
+    }
+  }
+
+  late T _state;
+
+  /// Current state
+  T get state => _state;
+
+  set state(T val) => modify(val);
+
+  void Function(T val)? onUpdate;
+
+  void modify(T val) {
+    try {
+      add(Change<T>(_state, () => _newValue(val), _newValue));
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  void _newValue(T val) {
+    _state = val;
+
+    if (onUpdate != null) {
+      onUpdate!(val);
+    }
+  }
+}
